cd42802e0a8bb7f0ffd6ef6c93183d9501b29b8a
Mikhail <miksolodovnikov@gmail.com>
2017-01-13 21:05:21 +0500

    Add possibility to remove list and nodes to avoid memory leaks. Add few new test cases.

--- linkedLists/linked_list.c
+++ linkedLists/linked_list.c
@@ -50,10 +50,52 @@ void printList() {
     printf("]\n");
 }
 
+void removeList() {
+    if (head == NULL) {
+        printf("There are no head node defined!\n");
+        return;
+    }
+    struct Node* current = head;
+    while (current->next != NULL) {
+        struct Node* tmp = current->next;
+        free(current);
+        current = tmp;
+    }
+    head = NULL;
+    printf("List was removed!\n");
+}
+
+void removeNode(struct Node* node) {
+    if (head == node) {
+        printf("Head node\n");
+        struct Node* tmp = head;
+        head = head->next;
+        free(head);
+    } else {
+        node->prev->next = node->next;
+        if (node->next != NULL) node->next->prev = node->prev;
+        free(node);
+    }
+    printf("Node was removed!\n");
+}
+
 
 int main()
 {
+    removeList();
     head = createNode(0);
+    printList();
+
+    head = createNode(5);
+    printList();
+    removeNode(head);
+
+    head = createNode(5);
+    printList();
+    insertToTail(6);
+    printList();
+    struct Node* n = head->next;
+    removeNode(n);
     printList();
 
     for (int i=1; i<10; ++i) {
@@ -64,14 +106,15 @@ int main()
         }
     }
     printList();
+    removeList();
     
-    head = NULL;
     insertToHead(100);
     printList();
+    removeList();
 
-    head = NULL;
     insertToTail(101);
     printList();
+    removeList();
 
     return 0;
 }
