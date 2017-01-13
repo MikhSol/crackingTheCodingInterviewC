a9b7ef400ff60b278d76c20c272920294aa33c0f
Mikhail <miksolodovnikov@gmail.com>
2017-01-13 18:43:14 +0500

    Initial commit. Add simple failing test.

---
+++ linkedLists/linked_lists_test.c
@@ -0,0 +1,10 @@
+//tests for linked lists tasks from the "Cracking The Coding Interview".
+
+#include <assert.h>
+#include <stdbool.h>
+
+int main()
+{
+  assert(false && "First unit test");
+  return 0;
+}
